faf17491014441ac17db89137d506d4f0d18d8b7
tura <soldier4443@gmail.com>
2018-04-09 23:00:50 +0900

    Initial Commit

---
+++ app/src/main/java/com/turastory/jamquery/MainEntry.java
@@ -0,0 +1,13 @@
+package com.turastory.jamquery;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainEntry extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_entry);
+    }
+}
