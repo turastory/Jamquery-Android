86407841778a9544b770e99e2c3d5656429b5e35
tura <soldier4443@gmail.com>
2018-04-11 22:43:39 +0900

    Presentation Layer 실패하는 테스트 작성

--- app/src/androidTest/java/com/turastory/jamquery/presentation/ui/jamquery_list/JamqueryListActivityTest.java
+++ app/src/androidTest/java/com/turastory/jamquery/presentation/ui/jamquery_list/JamqueryListActivityTest.java
@@ -10,12 +10,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Created by tura on 2018-04-11.
+ *
+ * TODO: Test clicking on the list item.
  */
 @RunWith(AndroidJUnit4.class)
 public class JamqueryListActivityTest {
@@ -25,6 +30,30 @@ public class JamqueryListActivityTest {
     
     @Test
     public void test_showEmptyViewWhenNotEditQuery() {
+        onView(withId(R.id.jamquery_list_empty_view))
+            .check(matches(isDisplayed()));
+    }
+    
+    @Test
+    public void test_hideEmptyViewWhenEditQuery() {
+        onView(withId(R.id.edit_text_query))
+            .perform(typeText("asdf"));
+        
+        onView(withId(R.id.jamquery_list_empty_view))
+            .check(matches(not(isDisplayed())));
+    }
+    
+    @Test
+    public void test_showEmptyViewAgainAfterClearEditQuery() {
+        onView(withId(R.id.edit_text_query))
+            .perform(typeText("asdf"));
+    
+        onView(withId(R.id.jamquery_list_empty_view))
+            .check(matches(not(isDisplayed())));
+    
+        onView(withId(R.id.edit_text_query))
+            .perform(clearText());
+    
         onView(withId(R.id.jamquery_list_empty_view))
             .check(matches(isDisplayed()));
     }
