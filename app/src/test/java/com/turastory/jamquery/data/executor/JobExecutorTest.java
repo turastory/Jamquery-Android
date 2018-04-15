afe8449cc055417ab1948480c952b1ac19b7fdf4
tura <soldier4443@gmail.com>
2018-04-15 17:03:09 +0900

    Executor 테스트할 때 생기는 문제 수정

--- app/src/test/java/com/turastory/jamquery/data/executor/JobExecutorTest.java
+++ app/src/test/java/com/turastory/jamquery/data/executor/JobExecutorTest.java
@@ -2,22 +2,57 @@ package com.turastory.jamquery.data.executor;
 
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 /**
  * Created by tura on 2018-04-15.
  */
 public class JobExecutorTest {
     
+    private AtomicInteger counter = new AtomicInteger(0);
+    
     @Test
     public void test_execute() {
-        Runnable mockRunnable = mock(Runnable.class);
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+    
+        executeAddOne(countDownLatch);
+    
+        try {
+            countDownLatch.await();
+            assertThat(counter.get(), is(1));
+        } catch (InterruptedException e) {
+            fail();
+            e.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void test_multipleExecutes() {
+        int executeCount = 5;
+        CountDownLatch countDownLatch = new CountDownLatch(executeCount);
         
-        JobExecutor.getInstance().execute(mockRunnable);
+        for (int i = 0; i < executeCount; i++)
+            executeAddOne(countDownLatch);
         
-        verify(mockRunnable).run();
+        try {
+            countDownLatch.await();
+            assertThat(counter.get(), is(executeCount));
+        } catch (InterruptedException e) {
+            fail();
+            e.printStackTrace();
+        }
+    }
+    
+    private void executeAddOne(CountDownLatch countDownLatch) {
+        JobExecutor.getInstance().execute(() -> {
+            counter.addAndGet(1);
+            countDownLatch.countDown();
+        });
     }
     
     @Test
--- app/src/test/java/com/turastory/jamquery/presentation/base/UIExecutorTest.java
+++ app/src/test/java/com/turastory/jamquery/presentation/base/UIExecutorTest.java
@@ -1,12 +1,19 @@
 package com.turastory.jamquery.presentation.base;
 
+import android.os.Looper;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.robolectric.Shadows.shadowOf;
 
 /**
  * Created by tura on 2018-04-15.
@@ -19,8 +26,26 @@ public class UIExecutorTest {
         Runnable mockRunnable = mock(Runnable.class);
         
         UIExecutor.getInstance().post(mockRunnable);
+        shadowOf(Looper.getMainLooper()).idle();
         
         verify(mockRunnable).run();
+    }
+    
+    @Test
+    public void test_multiplePosts() {
+        AtomicInteger counter = new AtomicInteger(0);
+        int testCount = 5;
+    
+        for (int i = 0; i < testCount; i++)
+            addOne(counter);
+    
+        shadowOf(Looper.getMainLooper()).idle();
+    
+        assertThat(counter.get(), is(testCount));
+    }
+    
+    private void addOne(AtomicInteger counter) {
+        UIExecutor.getInstance().post(() -> counter.addAndGet(1));
     }
     
     @Test
